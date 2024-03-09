d7e000a5690e58187b06810d13f9ce94950c60ea
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2024-03-10 00:44:35 +0530

    fix

--- src/middleware.js
+++ src/middleware.js
@@ -5,8 +5,7 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
   const path = request.nextUrl.pathname
 
-  const isPublicPath = path === '/login' || path === '/signup' || path === '/verifyemail' || path === '/' || path === '/user-details'
-
+  const isPublicPath = path === '/login' || path === '/signup' || path === '/'
   const token = request.cookies.get('token')?.value || ''
 
   if(isPublicPath && token) {
